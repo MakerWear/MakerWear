17aa6759f1981ab4f3f418200d7b2967edddeb73
Majeed <majeed@umd.edu>
2015-08-10 17:11:30 -0400

    Distance Sensor
    The output voltage is now affected by its input voltage.

--- Distance Sensor/DistanceSensorATtiny/DistanceSensorATtiny.ino
+++ Distance Sensor/DistanceSensorATtiny/DistanceSensorATtiny.ino
@@ -1,5 +1,7 @@
+//TODO: Add resistor values in the code
+
 //Pin Configurations
-//int input_pin = 3;           //Pin 2 on ATtiny
+int input_pin = 3;           //Pin 2 on ATtiny
 int ir_receiver_pin = 2;   //Pin 3 on ATtiny
 int output_pin = 1;          //Pin 6 on ATtiny
 
@@ -12,7 +14,10 @@ void setup() {
 void loop() {
   int x = analogRead(ir_receiver_pin);
   float y = .000003*x*x*x - 0.0007*x*x + 0.1079*x + 8.2076;
-  int out_value = map(y, 7, 41, 255, 0);
+  
+  int input_value = analogRead(input_pin);
+  
+  int out_value = map(y, 7, 41, input_value/4, 0);
 
   if (out_value < 9)
     out_value = 0;
