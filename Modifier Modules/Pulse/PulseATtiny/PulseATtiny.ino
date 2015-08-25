6ff689a96ca9588fade6231d9aa8a0982b35f296
Majeed <majeed@umd.edu>
2015-08-25 14:15:45 -0400

    added pulse module

--- Modifier Modules/Pulse/PulseATtiny/PulseATtiny.ino
+++ Modifier Modules/Pulse/PulseATtiny/PulseATtiny.ino
@@ -1,25 +1,61 @@
-//Pin Configurations
-int input_pin = 3; //Pin 2 on ATtiny
-int output_pin = 1; //Pin 6 on ATtiny
+/*
+**  PulseATtiny.ino
+**  MakerWear Pulse Module's ATtiny Program.
+**
+**  Pulses 5V in a frequency proportional to voltage input (higher voltage
+**  equals higher pulse rate)
+**
+**
+**  ATtiny85 Pin Configurations:  
+**
+**  Pin 1 (Reset): N/U                   Pin 8 (PWR):         5V
+**  Pin 2 (D3/A3): Module Input          Pin 7 (D2/A1/SCK):   N/U
+**  Pin 3 (D4/A2): N/U                   Pin 6 (D1/PWM/MISO): Module Output
+**  Pin 4 (GND):   GND                   Pin 5 (D0/PWM/MOSI): N/U
+**
+**
+**  Created on 8/10/15.
+**  By Majeed Kazemitabaar
+**  Modified on 8/25/15.
+**  By Majeed Kazemitabaar
+**
+**  MakerWear Link:
+**  Github Link:      github.com/myjeeed/MakerWear
+**
+*/
 
-//Constants
+#include <FilteredAnalogInput.h>
+
+int input_pin = 3;                           //Pin 2 on ATtiny
+int output_pin = 1;                          //Pin 6 on ATtiny
+int filter_size = 15;                        //Noise reduction filter size
 int min_period = 25;
 int max_period = 1000;
 
-void setup() {
+FilteredAnalogInput input(input_pin, filter_size);
+
+void setup() 
+{
   pinMode(output_pin, OUTPUT);
-  
-  //Just For Debugging:
-  //Serial.begin(9600);
 }
 
-void loop() {
-  int input_value = analogRead(input_pin);
-  int period = map(input_value, 0, 1023, max_period, min_period);
+void loop()
+{
+  int input_val = map(input.filteredAnalogRead(AVERAGE), 50, 975, 0, 1023);
+  int period;
   
+  if(input_val < 0)
+    input_val = 0;
+  else if(input_val > 1023)
+    input_val = 1023;
+
+  period = map(input_val, 0, 1023, max_period, min_period);
   digitalWrite(output_pin, HIGH);
+
   delay(period);
-  period = map(input_value, 0, 1023, max_period, min_period);
+
+  period = map(input_val, 0, 1023, max_period, min_period);
   digitalWrite(output_pin, LOW);
+
   delay(period);
 }
--- Modifier Modules/Pulse/PulseArduino/PulseArduino.ino
+++ Modifier Modules/Pulse/PulseArduino/PulseArduino.ino
@@ -1,17 +1,36 @@
-/* 
-** MakerWear Pulse Module
-** 
-** Written & Last Modified by Majeed Kazemitabaar
-** Friday, July 31th, 2015
+/*
+**  PulseArduino.ino
+**  MakerWear Pulse Module's Arduino Program.
+**
+**  Pulses 5V in a frequency proportional to voltage input (higher voltage
+**  equals higher pulse rate)
+**
+**
+**  Arduino Pin Configurations:  
+**
+**  Arduino Pin 11: Module Output
+**  Arduino Pin A0: Module Input
+**
+**
+**  Created on 8/10/15.
+**  By Majeed Kazemitabaar
+**  Modified on 8/25/15.
+**  By Majeed Kazemitabaar
+**
+**  MakerWear Link:
+**  Github Link:      github.com/myjeeed/MakerWear
+**
 */
+
+#include <FilteredAnalogInput.h>
  
-//Pin Configurations
 int input_pin = A0;
 int output_pin = 13;
-
-//Constants
+int filter_size = 15;                        //Noise reduction filter size
 int min_period = 25;
 int max_period = 1000;
+
+FilteredAnalogInput input(input_pin, filter_size);
 
 void setup() {
 
@@ -23,17 +42,21 @@ void setup() {
 }
 
 void loop() {
-  int input_value = analogRead(input_pin);
-  int period = map(input_value, 0, 1023, max_period, min_period);
+  int input_val = map(input.filteredAnalogRead(AVERAGE), 50, 975, 0, 1023);
+  int period;
   
+  if(input_val < 0)
+    input_val = 0;
+  else if(input_val > 1023)
+    input_val = 1023;
+
+  period = map(input_val, 0, 1023, max_period, min_period);
   digitalWrite(output_pin, HIGH);
+
   delay(period);
-  period = map(input_value, 0, 1023, max_period, min_period);
+
+  period = map(input_val, 0, 1023, max_period, min_period);
   digitalWrite(output_pin, LOW);
+
   delay(period);
 }
-
-/*
-** Changelog 
-**
-*/
