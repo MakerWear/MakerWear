d9638fb2c6882579ca9bcc77a30d0382fa05aac5
Majeed <majeed@umd.edu>
2015-08-25 10:36:23 -0400

    Wrote a bunch of libraries
    - Moving Average Filter + Hanning Filter
    - TinyWireM (from Arduino)
    - Used TinyWireM to interface the Flora Sensors (that use SPI) with the
    ATtiny85

---
+++ libraries/SignalProcessing/FilteredAnalogInput.cpp
@@ -0,0 +1,78 @@
+#include "FilteredAnalogInput.h"
+
+FilteredAnalogInput::FilteredAnalogInput(int pin_number, int window_size)
+{
+  if(window_size > MAX_SIZE)
+    this->qSize = MAX_SIZE;
+  else if(window_size <= 0)
+    this->qSize = 1;
+  else
+    this->qSize = window_size;
+    
+  this->qPush = -1;
+  this->qPop = -1;
+  
+  this->pin_number = pin_number;
+  
+  for(int i = 0; i < this->qSize; i++)
+    this->queue[i] = 0;
+    
+  this->alpha = 0.54;
+  this->beta = 0.46;
+  calculateHanningWeights();
+}
+
+
+void FilteredAnalogInput::calculateHanningWeights()
+{  
+  hanning_sum = 0;
+  
+  for(int i = 0; i < qSize; i++)
+  {
+    hanning_weights[i] = alpha - beta * (float)cos((2*3.14*i)/(qSize-1));
+    hanning_sum += hanning_weights[i];
+  }
+}
+
+
+int FilteredAnalogInput::filteredAnalogRead(int filter)
+{
+  int input_val = analogRead(this->pin_number);
+  float avg_val = 0.0;
+  
+  if(this->qPush != this->qPop)
+    //when the queue is still not full:
+    push(input_val);
+  else
+  {
+    pop();
+    push(input_val);
+  }
+  
+  if(filter == AVERAGE)
+  {
+    for(int i = 0; i < this->qSize; i++)
+      avg_val += this->queue[i];
+    
+    return avg_val/this->qSize; 
+  }
+  else if(filter == HANNING)
+  {
+    for(int i = 0; i < this->qSize; i++)
+      avg_val += this->queue[i]*hanning_weights[i];
+      
+    return avg_val/this->hanning_sum;
+  }
+}
+
+void FilteredAnalogInput::push(int x)
+{
+    qPush = (qPush + 1) % qSize;
+    queue[qPush] = x;
+}
+
+int FilteredAnalogInput::pop()
+{
+    qPop = (qPop + 1) % qSize;
+    return queue[qPop];
+}
