2d505411d8e58ed2f7be8a8d7fc1c643a2ee543b
Majeed <majeed@umd.edu>
2016-06-01 22:27:02 -0400

    still not working on ATtiny

--- Sensor Modules/Sound Sensor/SoundSensorATtiny/SoundSensorATtiny.ino
+++ Sensor Modules/Sound Sensor/SoundSensorATtiny/SoundSensorATtiny.ino
@@ -5,8 +5,10 @@
 **  Changes voltage output proportionality response to sound (higher voltage
 **  means higher sound)
 **
+**  Programmed with ATtiny (last option in the menu )
 **
-**  ATtiny Pin Configurations:  
+**
+**  ATtiny Pin Configurations:
 **
 **  Pin 1 (Reset): N/U                   Pin 8 (PWR):         5V
 **  Pin 2 (D3/A3): Module Input          Pin 7 (D2/A1/SCK):   N/U
@@ -22,54 +24,87 @@
 **
 */
 
-//#include <FilteredAnalogInput.h>
+#include <FilteredAnalogInput.h>
+//#include <SoftwareSerial.h>
 
-//int input_pin = 3;
+int input_pin = 3;
 int sound_sensor_pin = 2;
 int output_pin = 1;
-//int filter_size = 15;                        //Noise reduction filter size
+int filter_size = 15;                        //Noise reduction filter size
 
 const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
-unsigned int sample;
+int sample;
 
-//FilteredAnalogInput input(input_pin, filter_size);
+FilteredAnalogInput input(input_pin, filter_size);
 
-void setup() 
+//const int rx = -1;
+//const int tx = 2;
+
+//SoftwareSerial serial(rx, tx);
+
+
+void setup()
 {
+  //pinMode(rx, INPUT);
+  //pinMode(tx, OUTPUT);
+  //serial.begin(9600);
+  pinMode(output_pin, OUTPUT);
 }
 
 void loop() {
-  
-  unsigned long startMillis = millis();  // Start of sample window
-  unsigned int peakToPeak = 0;   // peak-to-peak level
-   
-  unsigned int signalMax = 0;
-  unsigned int signalMin = 1024;
-   
+  int input_val = map(input.filteredAnalogRead(AVERAGE), 50, 975, 0, 1023);
+
+  if (input_val < 0)
+    input_val = 0;
+  else if (input_val > 1023)
+    input_val = 1023;
+
+  long startMillis = millis();  // Start of sample window
+  int peakToPeak = 0;   // peak-to-peak level
+
+  int signalMax = 0;
+  int signalMin = 1024;
+
   // collect data for 50 mS
   while (millis() - startMillis < sampleWindow)
   {
-     sample = analogRead(sound_sensor_pin);
-     
-     if (sample < 1024)  // toss out spurious readings
-     {
-        if (sample > signalMax)
-        {
-           signalMax = sample;  // save just the max levels
-        }
-        else if (sample < signalMin)
-        {
-           signalMin = sample;  // save just the min levels
-        }
-     }
+    sample = analogRead(sound_sensor_pin);
+
+    //serial.println(sample);
+
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax)
+      {
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
   }
-  
-  
+
+
   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-  double volts = (peakToPeak * 10) / 1024;  // convert to volts
-  
-  
-  //TODO: add windowing filter to make the output signal smoother and be more stable.
-  int out_value = map(volts, 0.0, 10, 0, 255);
+  int volts = peakToPeak / 3;
+
+  //   double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+
+
+  // volts *= 100;
+
+  if (volts > 75)
+    volts = 75;
+  if (volts < 10)
+    volts = 10;
+
+
+  int out_value = map(volts, 10, 75, 0, input_val / 4);
   analogWrite(output_pin, out_value);
+
+  //serial.println(out_value);
+  
+  delay(100);
 }
