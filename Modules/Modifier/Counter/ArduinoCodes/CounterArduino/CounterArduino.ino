e3a36a2a21b80c0c990355a5a1f026f797f31b9f
alexjiao125 <alex.jiao97@gmail.com>
2016-06-14 12:57:01 -0400

    added counter module

--- Modules/Modifier/Counter/ArduinoCodes/CounterArduino/CounterArduino.ino
+++ Modules/Modifier/Counter/ArduinoCodes/CounterArduino/CounterArduino.ino
@@ -20,29 +20,45 @@
 **
 */
 
-#include <FilteredAnalogInput.h>
- 
+#include <SignalProcessing.h>
+
+//0-300 is LOW
+//700-1023 is HIGH
+
+#define THRESH_LOW 300
+#define THRESH_HIGH 700
+
 int input_pin = A0;
 int output_pin = 11;
 int filter_size = 15;                        //Noise reduction filter size
+short countable = 1; 
+unsigned int counter;
 
-FilteredAnalogInput input(input_pin, filter_size);
+SignalProcessing input(input_pin, filter_size);
 
 void setup()
 {
   pinMode(output_pin, OUTPUT);
   
   //Just For Debugging:
-  //Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-  int input_val = map(input.filteredAnalogRead(AVERAGE), 50, 975, 0, 1023);
-  
-  if(input_val < 0)
-    input_val = 0;
-  else if(input_val > 1023)
-    input_val = 1023;
-
+  int input_val = cutAndMap(input.filteredAnalogRead(AVERAGE), 50, 975, 0, 1023);
+ 
+  if((input_val > THRESH_HIGH) && (countable == 1)){
+    (counter==9)?(counter=0):(counter++);
+    countable = 0;   
+  }
+  else if((input_val < THRESH_LOW) && (countable == 0)){
+    countable = 1;
+  }
+  Serial.print("Countable: ");
+  (countable==1)?Serial.print("YES  "):Serial.print("NO  ");
+  Serial.print("Count: ");
+  Serial.println(counter);
+  analogWrite(output_pin, cutAndMap(counter, 0, 9, 0, 255));
+  delay(1);
 }
