a74f07567d14663fbac6f622c4b401bd8eb6f271
Majeed <majeed@umd.edu>
2016-06-24 16:22:31 -0400

    TemperatureReader: added base code

---
+++ Modules/Sensor/TemperatureReader/ArduinoCodes/TemperatureReaderATmega/TemperatureReaderATmega.ino
@@ -0,0 +1,63 @@
+/*
+**  ModuleNameATmega.ino
+**  MakerWear ModuleName Module's ATmega Program.
+**
+**  Description
+**  Description
+**  Description
+**
+**
+**  ATmega328 Pin Configurations:
+**
+**  Pin 1  (Reset):                          Pin 28  (A5):  
+**  Pin 2  (D0/RX):                          Pin 27  (A4):
+**  Pin 3  (D1/TX):                          Pin 26  (A3):
+**  Pin 4  (D2):                             Pin 25  (A2):
+**  Pin 5  (D3/PWM):                         Pin 24  (A1):
+**  Pin 6  (D4):                             Pin 23  (A0):       Module Input
+**  Pin 7  (PWR):                            Pin 22  (GND):
+**  Pin 8  (GND):                            Pin 21  (AREF):
+**  Pin 9  (XTAL1):                          Pin 20  (AVCC):
+**  Pin 10 (XTAL2):                          Pin 19  (D13):
+**  Pin 11 (D5/PWM):                         Pin 18  (D12):
+**  Pin 12 (D6/PWM):                         Pin 17  (D11/PWM):
+**  Pin 13 (D7):                             Pin 16  (D10/PWM):
+**  Pin 14 (D8):                             Pin 15  (D9/PWM):
+**
+**
+**  Created on xx/yy/zz.
+**  By Akbar Akbari
+**  Modified on xx/yy/zz.
+**  By Asghar Asghari
+**
+**  MakerWear Link:
+**  Github Link:      github.com/myjeeed/MakerWear
+**
+*/
+
+#include <SignalProcessing.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+int input_pin = A0;                         //pin 23 on ATmega328
+int filter_size = 15;                       //Noise reduction filter size
+int output_pin = 9;                         //pin PB1 (OC1A) PWM
+
+FilteredAnalogInput input(input_pin, filter_size);
+
+void setup()
+{
+  mlx.begin();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+    int input_val = cutAndMap(input.filteredAnalogRead(AVERAGE), 50, 975, 0, 1023);
+
+
+    int temperature = mlx.readObjectTempC();
+    Serial.println(temperature);
+}
