ab4e9706e338117c402178ea14a7c75a922eea38
Majeed <majeed@umd.edu>
2016-11-08 08:20:10 -0500

    trying to make the wireless sender module to communicate with a computer
    didn’t work :-/

---
+++ Modules/Misc/Wireless/ArduinoCodes/ReceiverWireless/ReceiverWireless.ino
@@ -0,0 +1,74 @@
+
+/*
+* Getting Started example sketch for nRF24L01+ radios
+* This is a very basic example of how to send data from one node to another
+* Updated: Dec 2014 by TMRh20
+*/
+
+#include <SPI.h>
+#include "RF24.h"
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 1;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+/**********************************************************/
+
+byte addresses[][6] = {"1Node","2Node"};
+
+// Used to control whether this node is sending or receiving
+bool role = 0;
+
+#include <SignalProcessing.h>
+
+const int input_pin = A0;       // pin 23 on ATmega328
+const int filter_size = 10;     // Noise reduction filter size                        
+
+SignalProcessing input(input_pin, filter_size);
+
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+ // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  
+  // Start the radio listening for data
+  radio.startListening();
+}
+
+int input_val;
+
+void loop() {
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+    
+    if ( timeout )                                             // Describe the results
+        Serial.println("Failed, response timed out.");
+    else{
+        int received_val;                                 // Grab the response, compare, and send to debugging spew
+        radio.read( &received_val, sizeof(int) );
+
+        Serial.println(received_val);
+    }
+}
+
---
+++ Modules/Misc/Wireless/ArduinoCodes/SenderWireless/SenderWireless.ino
@@ -0,0 +1,68 @@
+
+/*
+* Getting Started example sketch for nRF24L01+ radios
+* This is a very basic example of how to send data from one node to another
+* Updated: Dec 2014 by TMRh20
+*/
+
+#include <SPI.h>
+#include "RF24.h"
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 0;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+/**********************************************************/
+
+byte addresses[][6] = {"1Node","2Node"};
+
+// Used to control whether this node is sending or receiving
+bool role = 0;
+
+#include <SignalProcessing.h>
+
+const int input_pin = A0;       // pin 23 on ATmega328
+const int filter_size = 10;     // Noise reduction filter size                        
+
+SignalProcessing input(input_pin, filter_size);
+
+void setup() {
+  
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+ // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  
+  // Start the radio listening for data
+  radio.stopListening();
+}
+
+int input_val;
+
+void loop() {
+
+  input_val = cutAndMap(input.filteredAnalogRead(AVERAGE), 50, 900, 0, 1023);  
+  
+  if (role == 1)  {
+    //Now sending
+     if (!radio.write( &input_val, sizeof(int) )){
+       //print failed
+     }
+     //otherwise success 
+  }
+
+  delay(50);
+} // Loop
+
